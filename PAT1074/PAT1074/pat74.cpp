64495794bd382ecc2f79892f878159d830fe511d
Madalar <33518504+Mazziruso@users.noreply.github.com>
2018-10-31 09:42:29 +0800

    hash table

--- PAT1074/PAT1074/pat74.cpp
+++ PAT1074/PAT1074/pat74.cpp
@@ -9,12 +9,27 @@ using namespace std;
 struct node {
 	int addr;
 	int key;
-	node(int add, int k) : addr(add), key(k) {};
-	node() : addr(0), key(0) {};
+	int next;
+	node() : addr(-1), key(0), next(-1) {};
 };
 
-map<int, int> addrID;
-node* List;
+node *List;
+
+inline void swap(int i, int j) {
+	node tmp = List[i];
+	List[i] = List[j];
+	List[j] = tmp;
+}
+
+void reverseList(int i, int j) {
+	int l = i;
+	int r = j;
+	while (l < r) {
+		swap(l, r);
+		l++;
+		r--;
+	}
+}
 
 int main() {
 
@@ -23,20 +38,42 @@ int main() {
 
 	scanf_s("%d %d %d", &root, &N, &K);
 
+	node *nodes = new node[1000010]; //linear hash table stores nodes;
 	List = new node[N];
-	node* nodes = new node[N];
 
 	int key;
 	int next;
 	int addr;
 	for (int i = 0; i < N; i++) {
 		scanf_s("%d %d %d", &addr, &key, &next);
-		nodes[i].addr = addr;
-		nodes[i].key = key;
-		addrID[addr] = i;
+		nodes[addr].addr = addr;
+		nodes[addr].key = key;
+		nodes[addr].next = next;
 	}
 
 	//construct List;
+	int index = 0;
+	addr = root;
+	while (addr != -1) {
+		if (nodes[addr].addr == -1) {
+			break;
+		}
+		List[index] = nodes[addr];
+		addr = List[index].next;
+		if (index%K == K - 1) {
+			reverseList(index - K + 1, index);
+		}
+		index++;
+	}
+
+	for (int j = 0; j < index; j++) {
+		if (j < index - 1) {
+			printf("%05d %d %05d\n", List[j].addr, List[j].key, List[j + 1].addr);
+		}
+		else {
+			printf("%05d %d -1\n", List[j].addr, List[j].key);
+		}
+	}
 
 	system("pause");
 	return 0;
